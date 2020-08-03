9d3c22cb4f41ad40c564dd82cfa932d225ceb600
DinethP <dineth_perera@hotmail.com>
2020-08-03 13:37:41 +0530

    Chromedriver working on WSL 1

--- amazon_config.py
+++ amazon_config.py
@@ -1,4 +1,4 @@
-  selenium import webdriver
+from  selenium import webdriver
 
 DIRECTORY = 'reports'
 NAME = 'PS4'
@@ -6,24 +6,24 @@ CURRENCY = '€'
 MIN_PRICE = '275'
 MAX_PRICE = '650'
 FILTERS = {
-    'min': MIN_PRICE,
-    'max': MAX_PRICE
+  'min': MIN_PRICE,
+  'max': MAX_PRICE
 }
 BASE_URL = "http://www.amazon.de/"
 
-def get_chrome_web_driver(options):
-    return webdriver.Chrome('./chromedriver', chrome_options = options)
-
 def get_web_driver_options():
-    return webdriver.ChromeOptions()
+  return webdriver.ChromeOptions()
+
+def get_chrome_web_driver(options):
+  return webdriver.Chrome('./chromedriver.exe', chrome_options = options)
 
 def set_ignore_certificate_error(options):
-    options.add_argument('--ignore-certificate-errors')
+  options.add_argument('--ignore-certificate-errors')
 
 
 def set_browser_as_incognito(options):
-    options.add_argument('--incognito')
+  options.add_argument('--incognito')
 
 
 def set_automation_as_head_less(options):
-    options.add_argument('--headless')  
+  options.add_argument('--headless')  
--- simple_tracker.py
+++ simple_tracker.py
@@ -1,10 +1,44 @@
+from amazon_config import(
+  get_web_driver_options,
+  get_chrome_web_driver,
+  set_ignore_certificate_error,
+  set_browser_as_incognito,
+  NAME,
+  CURRENCY,
+  FILTERS,
+  BASE_URL,
+  DIRECTORY
+)
+
 class GenerateReport:
-    def __init__(self):
-        pass
+  def __init__(self):
+    pass
 
 class AmazonAPI:
-    def __init__(self):
-        pass
+  def __init__(self, search_term, filters, base_url, currency):
+    self.base_url = base_url
+    self.search_term = search_term  # This is the NAME parameter that is passed when AmazonAPI() is called in main function
+    options = get_web_driver_options() #Fetch the options
+    set_ignore_certificate_error(options)
+    set_browser_as_incognito(options)
+    self.driver = get_chrome_web_driver(options)
+    self.currency = currency
+    self.price_filter = f"&rh=p_36%3A{filters['min']}00-{filters['max']}00"   # Filter the URL to set min/max price
+    pass
+
+  def run(self):
+    print("Starting script...")
+    print(f"Looking fir {self.search_term} prodcuts...")
+    links = self.get_products_links()
+
+    self.driver.quit()
+
+  def get_products_links(self):
+    self.driver.get(self.base_url)
+
 
 if __name__ == '__main__':
-    print("Hi")
+  print("Hi")
+  amazon = AmazonAPI(NAME, FILTERS, BASE_URL, CURRENCY)
+  print(amazon.price_filter)
+  amazon.run()
